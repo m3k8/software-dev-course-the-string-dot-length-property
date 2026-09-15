3f093abd25767982f23398f5e8cc2916f5f7c936
m3k8 <154777955+m3k8@users.noreply.github.com>
2026-09-15 17:05:30 -0400

    Update the-string-dot-length-property.js
    Complete string length practice

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -20,11 +20,13 @@ let stringTwo = "JavaScript";
 let stringThree = "";
 
 // Your task: Assign the length of each string to the corresponding variable
-let lengthOne;   // Length of stringOne
-let lengthTwo;   // Length of stringTwo
-let lengthThree; // Length of stringThree
+let lengthOne = stringOne.length;
+let lengthTwo = stringTwo.length;
+let lengthThree = stringThree.length;
 
 // Your code here
 
-
+console.log(lengthOne);
+console.log(lengthTwo);
+console.log(lengthThree);
 
